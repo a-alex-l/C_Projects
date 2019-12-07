f8ef7a83936a69192d6943efd8fa026eda3b4a40
a-alex-l <40340659+a-alex-l@users.noreply.github.com>
2019-12-07 13:39:11 +0300

    Update main.cpp

--- XO_game/src/main.cpp
+++ XO_game/src/main.cpp
@@ -1,15 +1,7 @@
-#include <iostream>
 #include "StdioBoardView.hpp"
 #include "BoardTest.hpp"
 
-using namespace std;
-
-
 int main() {
-    BoardTest test;
-    test.run_all_tests();
-    test.show_final_result();
-
     StdioBoardView game;
     game.do_game_cycle();
 }
